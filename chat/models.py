bcd7c988b7a924e431bc7f353a1e2f74e3b72e4c
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-10-26 17:18:31 +0700

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Chat(models.Model):
-    mess = models.CharField(max_length=1000, default='Seen', blank=True, null=True)
+    mess = models.TextField( default='Seen', blank=True, null=True)
     created = models.DateTimeField( auto_now=True)
